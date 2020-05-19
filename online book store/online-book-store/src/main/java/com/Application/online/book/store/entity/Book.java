9d948fc3148e0cb919c590beea98e042c62458a9
vamsi <vamsi@192.168.2.18>
2020-05-19 17:24:59 +0530

    Vinay commit changes

--- online book store/online-book-store/src/main/java/com/Application/online/book/store/entity/Book.java
+++ online book store/online-book-store/src/main/java/com/Application/online/book/store/entity/Book.java
@@ -1,9 +1,15 @@
 package com.Application.online.book.store.entity;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -11,16 +17,42 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name="tblr_book")
+@Table(name="tbl_book")
 @Setter
 @Getter
 @ToString
 public class Book {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	private String sku;
+	
 	private String name;
+	
+	private String description;
+	
+	@Column(name="unit_price")
+	private BigDecimal unitPrice;
+	
+	@Column(name="image_url")
+	private String imageUrl;
+	
+	private boolean active;
+	
+	@Column(name="units_in_stock")
+	private int unitsInStock;
+	
+	@Column(name="date_created")
+	private Date createdOn;
+	
+	@Column(name="date_updated")
+	private Date updatedOn;
+	
+	@ManyToMany
+	@JoinColumn(name="category_id", nullable=false)
+	private BookCategory category;
+	
 
 }
